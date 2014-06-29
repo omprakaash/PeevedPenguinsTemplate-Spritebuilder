006c0dd500d428bbe2cc65476583fac40fdb16bd
omprakaash <omprakaash@Oms-MacBook-Pro.local>
2014-06-29 11:48:06 +0530

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/AppDelegate.m
+++ PeevedPenguins.spritebuilder/Source/AppDelegate.m
@@ -53,7 +53,8 @@
     //[cocos2dSetup setObject:kEAGLColorFormatRGB565 forKey:CCConfigPixelFormat];
     
     [self setupCocos2dWithOptions:cocos2dSetup];
-    
+    [CCBReader load:@"Penguin"];
+    [CCBReader load:@"Seal"];
     return YES;
 }
 
@@ -61,5 +62,6 @@
 {
     return [CCBReader loadAsScene:@"MainScene"];
 }
+
 
 @end
